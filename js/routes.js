492610e54cee3809d117d82997287a31c3190ba4
h4llowOs <comicalhallow@gmail.com>
2026-06-18 19:04:54 -0700

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,10 +1,12 @@
+import Home from './pages/Home.js';
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import Upcoming from './pages/Upcoming.js';
 
 export default [
-    { path: '/', component: List },
+    { path: '/', component: Home },
+    { path: '/list', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
     { path: '/upcoming', component: Upcoming },
