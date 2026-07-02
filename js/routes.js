10b09315bde764902e19a26159e30e33b0d6fba2
h4llowOs <comicalhallow@gmail.com>
2026-07-01 20:59:48 -0700

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,7 +1,6 @@
 import Home from './pages/Home.js';
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
-import Roulette from './pages/Roulette.js';
 import Upcoming from './pages/Upcoming.js';
 
 export default [
